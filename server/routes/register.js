f3b37c0e64027ac2ec5b65a336a00b56506384ad
Tamilarasankumar <98514024+Tamilarasankumar@users.noreply.github.com>
2024-02-10 20:23:18 +0530

    Update register.js 2

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const EmployeeModel = require('../models/Employee');
 
 const router = express.Router();
